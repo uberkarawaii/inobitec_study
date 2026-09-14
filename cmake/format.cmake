aaa1e3641c5415871d6b8a74885f612333a37f5c
uberkarawaii <antw0rt148@gmail.com>
2026-09-14 19:31:09 +0300

    checked CMake files encoding and fixed ed-config synaxis, added .cmake in it

--- cmake/format.cmake
+++ cmake/format.cmake
@@ -1,54 +1,54 @@
-# применение clang-format ко всем C/C++ файлам репозитория
-# проверка
-# cmake -DMODE=check -DSRC_DIR=<корень> -DBUILD_DIR=<каталог сборки> -P cmake/format.cmake
-# отформатировать
-# cmake -DMODE=apply -DSRC_DIR=<корень> -DBUILD_DIR=<каталог сборки> -P cmake/format.cmake
-# graceful decay - если на машине нет clang-format, об этом будет предупреждение, и тесты пройдут
-# если есть но не отформатировано - будет падение
+# РїСЂРёРјРµРЅРµРЅРёРµ clang-format РєРѕ РІСЃРµРј C/C++ С„Р°Р№Р»Р°Рј СЂРµРїРѕР·РёС‚РѕСЂРёСЏ
+# РїСЂРѕРІРµСЂРєР°
+# cmake -DMODE=check -DSRC_DIR=<РєРѕСЂРµРЅСЊ> -DBUILD_DIR=<РєР°С‚Р°Р»РѕРі СЃР±РѕСЂРєРё> -P cmake/format.cmake
+# РѕС‚С„РѕСЂРјР°С‚РёСЂРѕРІР°С‚СЊ
+# cmake -DMODE=apply -DSRC_DIR=<РєРѕСЂРµРЅСЊ> -DBUILD_DIR=<РєР°С‚Р°Р»РѕРі СЃР±РѕСЂРєРё> -P cmake/format.cmake
+# graceful decay - РµСЃР»Рё РЅР° РјР°С€РёРЅРµ РЅРµС‚ clang-format, РѕР± СЌС‚РѕРј Р±СѓРґРµС‚ РїСЂРµРґСѓРїСЂРµР¶РґРµРЅРёРµ, Рё С‚РµСЃС‚С‹ РїСЂРѕР№РґСѓС‚
+# РµСЃР»Рё РµСЃС‚СЊ РЅРѕ РЅРµ РѕС‚С„РѕСЂРјР°С‚РёСЂРѕРІР°РЅРѕ - Р±СѓРґРµС‚ РїР°РґРµРЅРёРµ
 
-# проверка что режим передали
+# РїСЂРѕРІРµСЂРєР° С‡С‚Рѕ СЂРµР¶РёРј РїРµСЂРµРґР°Р»Рё
 if(NOT DEFINED MODE OR (NOT "${MODE}" STREQUAL "check" AND NOT "${MODE}" STREQUAL "apply"))
     message(FATAL_ERROR "MODE must be 'check' or 'apply'; received: '${MODE}'")
 endif()
-# проверка что указали папку, в которой форматировать файлы
+# РїСЂРѕРІРµСЂРєР° С‡С‚Рѕ СѓРєР°Р·Р°Р»Рё РїР°РїРєСѓ, РІ РєРѕС‚РѕСЂРѕР№ С„РѕСЂРјР°С‚РёСЂРѕРІР°С‚СЊ С„Р°Р№Р»С‹
 if(NOT DEFINED SRC_DIR OR "${SRC_DIR}" STREQUAL "")
     message(FATAL_ERROR "SRC_DIR is not set")
 endif()
-# по умолчанию каталог сборки - корень
+# РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ РєР°С‚Р°Р»РѕРі СЃР±РѕСЂРєРё - РєРѕСЂРµРЅСЊ
 if(NOT DEFINED BUILD_DIR)
     set(BUILD_DIR "")
 endif()
 
-# поиск clang-format-a
+# РїРѕРёСЃРє clang-format-a
 find_program(CLANG_FORMAT NAMES clang-format)
 
-# если нет - предупреждаем и идём к тестам
+# РµСЃР»Рё РЅРµС‚ - РїСЂРµРґСѓРїСЂРµР¶РґР°РµРј Рё РёРґС‘Рј Рє С‚РµСЃС‚Р°Рј
 if(NOT CLANG_FORMAT)
     message(WARNING "clang-format was not found, so the file format was not checked. Install it to enable this check: "
                     "winget install --id=LLVM.ClangFormat -e (Windows) / sudo apt install clang-format (Linux)")
     return()
 endif()
 
-# список проверяемых / форматируемых файлов
+# СЃРїРёСЃРѕРє РїСЂРѕРІРµСЂСЏРµРјС‹С… / С„РѕСЂРјР°С‚РёСЂСѓРµРјС‹С… С„Р°Р№Р»РѕРІ
 file(GLOB_RECURSE FORMAT_FILES
     "${SRC_DIR}/*.c"
     "${SRC_DIR}/*.cpp"
     "${SRC_DIR}/*.h"
     "${SRC_DIR}/*.hpp")
 
-# исключить поддиректорию билда из ресурсов для фоматирования, т.к. там не программы, а их продукты 
-# а также продукты генерации CMake - не надо форматировать этот код
-# не дать исключить корень исходников, если кто-то соберёт "в корне"
+# РёСЃРєР»СЋС‡РёС‚СЊ РїРѕРґРґРёСЂРµРєС‚РѕСЂРёСЋ Р±РёР»РґР° РёР· СЂРµСЃСѓСЂСЃРѕРІ РґР»СЏ С„РѕРјР°С‚РёСЂРѕРІР°РЅРёСЏ, С‚.Рє. С‚Р°Рј РЅРµ РїСЂРѕРіСЂР°РјРјС‹, Р° РёС… РїСЂРѕРґСѓРєС‚С‹ 
+# Р° С‚Р°РєР¶Рµ РїСЂРѕРґСѓРєС‚С‹ РіРµРЅРµСЂР°С†РёРё CMake - РЅРµ РЅР°РґРѕ С„РѕСЂРјР°С‚РёСЂРѕРІР°С‚СЊ СЌС‚РѕС‚ РєРѕРґ
+# РЅРµ РґР°С‚СЊ РёСЃРєР»СЋС‡РёС‚СЊ РєРѕСЂРµРЅСЊ РёСЃС…РѕРґРЅРёРєРѕРІ, РµСЃР»Рё РєС‚Рѕ-С‚Рѕ СЃРѕР±РµСЂС‘С‚ "РІ РєРѕСЂРЅРµ"
 if(NOT "${BUILD_DIR}" STREQUAL "" AND NOT "${BUILD_DIR}" STREQUAL "${SRC_DIR}")
     list(FILTER FORMAT_FILES EXCLUDE REGEX "^${BUILD_DIR}/")
 endif()
 
-# если нечего форматировать - кинуть предупреждение
+# РµСЃР»Рё РЅРµС‡РµРіРѕ С„РѕСЂРјР°С‚РёСЂРѕРІР°С‚СЊ - РєРёРЅСѓС‚СЊ РїСЂРµРґСѓРїСЂРµР¶РґРµРЅРёРµ
 if(FORMAT_FILES STREQUAL "")
     message(WARNING "No C/C++ source files found under ${SRC_DIR}")
     return()
 endif()
-# форматирование
+# С„РѕСЂРјР°С‚РёСЂРѕРІР°РЅРёРµ
 if("${MODE}" STREQUAL "apply")
     execute_process(COMMAND "${CLANG_FORMAT}" -i ${FORMAT_FILES}
                     RESULT_VARIABLE result)
@@ -56,7 +56,7 @@ if("${MODE}" STREQUAL "apply")
         message(FATAL_ERROR "clang-format failed to format the files (exit code ${result})")
     endif()
     message(STATUS "clang-format: files formatted")
-# проверка формата
+# РїСЂРѕРІРµСЂРєР° С„РѕСЂРјР°С‚Р°
 else()
     execute_process(COMMAND "${CLANG_FORMAT}" --dry-run -Werror ${FORMAT_FILES}
                     RESULT_VARIABLE result)
--- tests/cases.cmake
+++ tests/cases.cmake
@@ -1,4 +1,4 @@
-# в переменных: exit-коды, out/err потоки как 1/2, пустые аргументы для программы
+# РІ РїРµСЂРµРјРµРЅРЅС‹С…: exit-РєРѕРґС‹, out/err РїРѕС‚РѕРєРё РєР°Рє 1/2, РїСѓСЃС‚С‹Рµ Р°СЂРіСѓРјРµРЅС‚С‹ РґР»СЏ РїСЂРѕРіСЂР°РјРјС‹
 set(SUCCESS 0)
 set(USAGE 64)
 set(DATA 65)
@@ -7,35 +7,35 @@ set(IO_FAIL 74)
 set(OUT 1)
 set(ERR 2)
 set(EMPTY_ARG "")
-# для достижения io-fail
+# РґР»СЏ РґРѕСЃС‚РёР¶РµРЅРёСЏ io-fail
 set(FOLDER_INPUT "${CMAKE_CURRENT_SOURCE_DIR}/input_data")
 
 # === T1 C TESTS ====
-# входные данные с ошибками
+# РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ СЃ РѕС€РёР±РєР°РјРё
 add_case(t1_c_empty t1_c "${EMPTY_ARG}" t1_nul "${NO_INPUT}" CONTAINS t1_nul "${ERR}")
 add_case(t1_c_abc t1_c "${EMPTY_ARG}" t1_abc "${DATA}" CONTAINS t1_abc "${ERR}")
 add_case(t1_c_float t1_c "${EMPTY_ARG}" t1_float "${DATA}" CONTAINS t1_float "${ERR}")
 add_case(t1_c_high t1_c "${EMPTY_ARG}" t1_high "${USAGE}" CONTAINS t1_high "${ERR}")
 add_case(t1_c_double_plus t1_c "${EMPTY_ARG}" t1_double_plus "${DATA}" CONTAINS t1_double_plus "${ERR}")
 add_case(t1_c_plus_abc t1_c "${EMPTY_ARG}" t1_plus_abc "${DATA}" CONTAINS t1_plus_abc "${ERR}")
-# нормальные входные данные
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ
 add_case(t1_c_norm t1_c "${EMPTY_ARG}" t1_test1 "${SUCCESS}" EQ t1_test1 "${OUT}")
 add_case(t1_c_plus t1_c "${EMPTY_ARG}" t1_plus "${SUCCESS}" EQ t1_test1 "${OUT}")
 
 # === T1 CPP TESTS ====
-# входные данные с ошибками
+# РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ СЃ РѕС€РёР±РєР°РјРё
 add_case(t1_cpp_empty t1_cpp "${EMPTY_ARG}" t1_nul "${NO_INPUT}" CONTAINS t1_nul "${ERR}")
 add_case(t1_cpp_abc t1_cpp "${EMPTY_ARG}" t1_abc "${DATA}" CONTAINS t1_abc "${ERR}")
 add_case(t1_cpp_float t1_cpp "${EMPTY_ARG}" t1_float "${DATA}" CONTAINS t1_float "${ERR}")
 add_case(t1_cpp_high t1_cpp "${EMPTY_ARG}" t1_high "${USAGE}" CONTAINS t1_high "${ERR}")
 add_case(t1_cpp_double_plus t1_cpp "${EMPTY_ARG}" t1_double_plus "${DATA}" CONTAINS t1_double_plus "${ERR}")
 add_case(t1_cpp_plus_abc t1_cpp "${EMPTY_ARG}" t1_plus_abc "${DATA}" CONTAINS t1_plus_abc "${ERR}")
-# нормальные входные данные
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ
 add_case(t1_cpp_norm t1_cpp "${EMPTY_ARG}" t1_test1 "${SUCCESS}" EQ t1_test1 "${OUT}")
 add_case(t1_cpp_plus t1_cpp "${EMPTY_ARG}" t1_plus "${SUCCESS}" EQ t1_test1 "${OUT}")
 
 # === T2 C TESTS ===
-# входные данные с ошибками
+# РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ СЃ РѕС€РёР±РєР°РјРё
 add_case(t2_c_eof_name t2_c "${EMPTY_ARG}" t2_eof_name "${NO_INPUT}" CONTAINS t2_eof_name "${ERR}")
 add_case(t2_c_empty_name t2_c "${EMPTY_ARG}" t2_empty_name "${DATA}" CONTAINS t2_empty_name "${ERR}")
 add_case(t2_c_eof_vertexes t2_c "${EMPTY_ARG}" t2_eof_vertexes "${NO_INPUT}" CONTAINS t2_eof_vertexes "${ERR}")
@@ -45,12 +45,12 @@ add_case(t2_c_nan_vertexes t2_c "${EMPTY_ARG}" t2_nan_vertexes "${DATA}" CONTAIN
 add_case(t2_c_negative_vertexes t2_c "${EMPTY_ARG}" t2_negative_vertexes "${USAGE}" CONTAINS t2_negative_vertexes "${ERR}")
 add_case(t2_c_double_plus t2_c "${EMPTY_ARG}" t2_double_plus "${DATA}" CONTAINS t2_double_plus "${ERR}")
 add_case(t2_c_plus_abc t2_c "${EMPTY_ARG}" t2_plus_abc "${DATA}" CONTAINS t2_plus_abc "${ERR}")
-# нормальные входные данные
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ
 add_case(t2_c_norm t2_c "${EMPTY_ARG}" t2_input "${SUCCESS}" EQ t2_norm "${OUT}")
 add_case(t2_c_plus t2_c "${EMPTY_ARG}" t2_plus "${SUCCESS}" EQ t2_norm "${OUT}")
 
 # === T2 CPP TESTS ===
-# входные данные с ошибками
+# РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ СЃ РѕС€РёР±РєР°РјРё
 add_case(t2_cpp_eof_name t2_cpp "${EMPTY_ARG}" t2_eof_name "${NO_INPUT}" CONTAINS t2_eof_name "${ERR}")
 add_case(t2_cpp_empty_name t2_cpp "${EMPTY_ARG}" t2_empty_name "${DATA}" CONTAINS t2_empty_name "${ERR}")
 add_case(t2_cpp_eof_vertexes t2_cpp "${EMPTY_ARG}" t2_eof_vertexes "${NO_INPUT}" CONTAINS t2_eof_vertexes "${ERR}")
@@ -60,16 +60,16 @@ add_case(t2_cpp_nan_vertexes t2_cpp "${EMPTY_ARG}" t2_nan_vertexes "${DATA}" CON
 add_case(t2_cpp_negative_vertexes t2_cpp "${EMPTY_ARG}" t2_negative_vertexes "${USAGE}" CONTAINS t2_negative_vertexes "${ERR}")
 add_case(t2_cpp_double_plus t2_cpp "${EMPTY_ARG}" t2_double_plus "${DATA}" CONTAINS t2_double_plus "${ERR}")
 add_case(t2_cpp_plus_abc t2_cpp "${EMPTY_ARG}" t2_plus_abc "${DATA}" CONTAINS t2_plus_abc "${ERR}")
-# нормальные входные данные
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ
 add_case(t2_cpp_norm t2_cpp "${EMPTY_ARG}" t2_input "${SUCCESS}" EQ t2_norm "${OUT}")
 add_case(t2_cpp_plus t2_cpp "${EMPTY_ARG}" t2_plus "${SUCCESS}" EQ t2_norm "${OUT}")
 
 # === T3 C TESTS ===
-# тест на io-fail - только под UNIX
+# С‚РµСЃС‚ РЅР° io-fail - С‚РѕР»СЊРєРѕ РїРѕРґ UNIX
 if(UNIX)
   add_case(t3_c_io t3_c "${EMPTY_ARG}" "${FOLDER_INPUT}" "${IO_FAIL}" CONTAINS t3_t4_io_fail "${ERR}")
 endif()
-# входные данные с ошибками
+# РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ СЃ РѕС€РёР±РєР°РјРё
 add_case(t3_c_empty t3_c "${EMPTY_ARG}" t3_empty_case "${NO_INPUT}" CONTAINS t3_empty_case "${ERR}")
 add_case(t3_c_not_digit_case t3_c "${EMPTY_ARG}" t3_not_digit_case "${DATA}" CONTAINS t3_not_digit_case "${ERR}")
 add_case(t3_c_too_few_args_case t3_c "${EMPTY_ARG}" t3_too_few_args_case "${DATA}" CONTAINS t3_too_few_args_case "${ERR}")
@@ -81,18 +81,18 @@ add_case(t3_c_x_ky_z t3_c "${EMPTY_ARG}" t3_x_ky_z "${DATA}" CONTAINS t3_x_ky_z 
 add_case(t3_c_x_yk_z t3_c "${EMPTY_ARG}" t3_x_yk_z "${DATA}" CONTAINS t3_x_yk_z "${ERR}")
 add_case(t3_c_double_plus t3_c "${EMPTY_ARG}" t3_double_plus "${DATA}" CONTAINS t3_double_plus "${ERR}")
 add_case(t3_c_plus_abc t3_c "${EMPTY_ARG}" t3_plus_abc "${DATA}" CONTAINS t3_plus_abc "${ERR}")
-# нормальные входные данные
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ
 add_case(t3_c_test1 t3_c "${EMPTY_ARG}" t3_test1 "${SUCCESS}" EQ t3_test1 "${OUT}")
 add_case(t3_c_test2 t3_c "${EMPTY_ARG}" t3_test2 "${SUCCESS}" EQ t3_test2 "${OUT}") 
 add_case(t3_c_test3 t3_c "${EMPTY_ARG}" t3_test3 "${SUCCESS}" EQ t3_test3 "${OUT}")
 add_case(t3_c_scientific t3_c "${EMPTY_ARG}" t3_scientific "${SUCCESS}" EQ t3_scientific "${OUT}")
 
 # === T3 CPP TESTS ===
-# тест на io-fail - только под UNIX
+# С‚РµСЃС‚ РЅР° io-fail - С‚РѕР»СЊРєРѕ РїРѕРґ UNIX
 if(UNIX)
   add_case(t3_cpp_io t3_cpp "${EMPTY_ARG}" "${FOLDER_INPUT}" "${IO_FAIL}" CONTAINS t3_t4_io_fail "${ERR}")
 endif()
-# входные данные с ошибками
+# РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ СЃ РѕС€РёР±РєР°РјРё
 add_case(t3_cpp_empty t3_cpp "${EMPTY_ARG}" t3_empty_case "${NO_INPUT}" CONTAINS t3_empty_case "${ERR}")
 add_case(t3_cpp_not_digit_case t3_cpp "${EMPTY_ARG}" t3_not_digit_case "${DATA}" CONTAINS t3_not_digit_case "${ERR}")
 add_case(t3_cpp_too_few_args_case t3_cpp "${EMPTY_ARG}" t3_too_few_args_case "${DATA}" CONTAINS t3_too_few_args_case "${ERR}")
@@ -104,13 +104,13 @@ add_case(t3_cpp_x_ky_z t3_cpp "${EMPTY_ARG}" t3_x_ky_z "${DATA}" CONTAINS t3_x_k
 add_case(t3_cpp_x_yk_z t3_cpp "${EMPTY_ARG}" t3_x_yk_z "${DATA}" CONTAINS t3_x_yk_z "${ERR}")
 add_case(t3_cpp_double_plus t3_cpp "${EMPTY_ARG}" t3_double_plus "${DATA}" CONTAINS t3_double_plus "${ERR}")
 add_case(t3_cpp_plus_abc t3_cpp "${EMPTY_ARG}" t3_plus_abc "${DATA}" CONTAINS t3_plus_abc "${ERR}")
-# нормальные входные данные
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ
 add_case(t3_cpp_test1 t3_cpp "${EMPTY_ARG}" t3_test1 "${SUCCESS}" EQ t3_test1 "${OUT}")
 add_case(t3_cpp_test2 t3_cpp "${EMPTY_ARG}" t3_test2 "${SUCCESS}" EQ t3_test2 "${OUT}") 
 add_case(t3_cpp_test3 t3_cpp "${EMPTY_ARG}" t3_test3 "${SUCCESS}" EQ t3_test3 "${OUT}")
 add_case(t3_cpp_scientific t3_cpp "${EMPTY_ARG}" t3_scientific "${SUCCESS}" EQ t3_scientific "${OUT}")
 
-# === РАДИУСЫ ===
+# === Р РђР”РРЈРЎР« ===
 set(RAD_TOO_MUCH 2 3)
 set(RAD_SYMB 4ch)
 set(RAD_INF inf)
@@ -125,12 +125,12 @@ set(RAD10 10)
 set(RAD100 100)
 
 # === T4 C TESTS ===
-# тест на io-fail - только под UNIX
+# С‚РµСЃС‚ РЅР° io-fail - С‚РѕР»СЊРєРѕ РїРѕРґ UNIX
 if(UNIX)
   add_case(t4_c_io t4_c "${RAD1}" "${FOLDER_INPUT}" "${IO_FAIL}" CONTAINS t3_t4_io_fail "${ERR}")
 endif()
-# плохие значения радиуса
-# t4_bad_radius - для наличия потока input, сам по себе не имеет значения
+# РїР»РѕС…РёРµ Р·РЅР°С‡РµРЅРёСЏ СЂР°РґРёСѓСЃР°
+# t4_bad_radius - РґР»СЏ РЅР°Р»РёС‡РёСЏ РїРѕС‚РѕРєР° input, СЃР°Рј РїРѕ СЃРµР±Рµ РЅРµ РёРјРµРµС‚ Р·РЅР°С‡РµРЅРёСЏ
 add_case(t4_c_radius_empty t4_c "${EMPTY_ARG}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_empty "${ERR}")
 add_case(t4_c_radius_too_much t4_c "${RAD_TOO_MUCH}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_too_much "${ERR}")
 add_case(t4_c_radius_symbol t4_c "${RAD_SYMB}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_symbol "${ERR}")
@@ -138,7 +138,7 @@ add_case(t4_c_radius_inf t4_c "${RAD_INF}" t4_bad_radius "${USAGE}" CONTAINS t4_
 add_case(t4_c_radius_negative t4_c "${RAD_NEG}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_negative "${ERR}")
 add_case(t4_c_radius_plus_plus t4_c "${RAD_PLUS_PLUS}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_plus_plus "${ERR}")
 add_case(t4_c_radius_plus_abc t4_c "${RAD_PLUS_ABC}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_plus_abc "${ERR}")
-# плохие точки
+# РїР»РѕС…РёРµ С‚РѕС‡РєРё
 add_case(t4_c_symbol_coords t4_c "${RAD1}" t4_symbol_coords "${DATA}" CONTAINS t4_symbol_coords "${ERR}")
 add_case(t4_c_too_few_coords t4_c "${RAD1}" t4_too_few_coords "${DATA}" CONTAINS t4_too_few_coords "${ERR}")
 add_case(t4_c_empty_coords t4_c "${RAD1}" t4_empty_coords "${NO_INPUT}" CONTAINS t4_empty_coords "${ERR}")
@@ -151,24 +151,24 @@ add_case(t4_c_x_ky_z t4_c "${RAD1}" t4_x_ky_z "${DATA}" CONTAINS t4_x_ky_z "${ER
 add_case(t4_c_x_yk_z t4_c "${RAD1}" t4_x_yk_z "${DATA}" CONTAINS t4_x_yk_z "${ERR}")
 add_case(t4_c_plus_plus_coord t4_c "${RAD1}" t4_plus_plus_coord "${DATA}" CONTAINS t4_plus_plus_coord "${ERR}")
 add_case(t4_c_plus_abc_coord t4_c "${RAD1}" t4_plus_abc_coord "${DATA}" CONTAINS t4_plus_abc_coord "${ERR}")
-# нормальные данные
-# единственная точка и она проходит
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РґР°РЅРЅС‹Рµ
+# РµРґРёРЅСЃС‚РІРµРЅРЅР°СЏ С‚РѕС‡РєР° Рё РѕРЅР° РїСЂРѕС…РѕРґРёС‚
 add_case(t4_c_test1 t4_c "${RAD8}" t4_test1 "${SUCCESS}" EQ t4_test1 "${OUT}")
-# много точек и только одна проходит
+# РјРЅРѕРіРѕ С‚РѕС‡РµРє Рё С‚РѕР»СЊРєРѕ РѕРґРЅР° РїСЂРѕС…РѕРґРёС‚
 add_case(t4_c_test2 t4_c "${RAD10}" t4_test2 "${SUCCESS}" EQ t4_test2 "${OUT}")
-# как прошлый тест, но радиус со знаком "+". результат и поведение должны совпасть
+# РєР°Рє РїСЂРѕС€Р»С‹Р№ С‚РµСЃС‚, РЅРѕ СЂР°РґРёСѓСЃ СЃРѕ Р·РЅР°РєРѕРј "+". СЂРµР·СѓР»СЊС‚Р°С‚ Рё РїРѕРІРµРґРµРЅРёРµ РґРѕР»Р¶РЅС‹ СЃРѕРІРїР°СЃС‚СЊ
 add_case(t4_c_plus t4_c "${RAD_POSITIVE}" t4_test2 "${SUCCESS}" EQ t4_test2 "${OUT}")
-# одна точка, но она не проходит
+# РѕРґРЅР° С‚РѕС‡РєР°, РЅРѕ РѕРЅР° РЅРµ РїСЂРѕС…РѕРґРёС‚
 add_case(t4_c_test3 t4_c "${RAD5}" t4_test3 "${SUCCESS}" EQ t4_test3 "${OUT}")
-# входные данные с минусом и экспонентой 
+# РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ СЃ РјРёРЅСѓСЃРѕРј Рё СЌРєСЃРїРѕРЅРµРЅС‚РѕР№ 
 add_case(t4_c_scientific t4_c "${RAD100}" t4_scientific "${SUCCESS}" EQ t4_scientific "${OUT}")
 
 # === T4 CPP TESTS ===
-# тест на io-fail - только под UNIX
+# С‚РµСЃС‚ РЅР° io-fail - С‚РѕР»СЊРєРѕ РїРѕРґ UNIX
 if(UNIX)
   add_case(t4_cpp_io t4_cpp "${RAD1}" "${FOLDER_INPUT}" "${IO_FAIL}" CONTAINS t3_t4_io_fail "${ERR}")
 endif()
-# плохие значения радиуса
+# РїР»РѕС…РёРµ Р·РЅР°С‡РµРЅРёСЏ СЂР°РґРёСѓСЃР°
 add_case(t4_cpp_radius_empty t4_cpp "${EMPTY_ARG}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_empty "${ERR}")
 add_case(t4_cpp_radius_too_much t4_cpp "${RAD_TOO_MUCH}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_too_much "${ERR}")
 add_case(t4_cpp_radius_symbol t4_cpp "${RAD_SYMB}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_symbol "${ERR}")
@@ -176,7 +176,7 @@ add_case(t4_cpp_radius_inf t4_cpp "${RAD_INF}" t4_bad_radius "${USAGE}" CONTAINS
 add_case(t4_cpp_radius_negative t4_cpp "${RAD_NEG}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_negative "${ERR}")
 add_case(t4_cpp_radius_plus_plus t4_cpp "${RAD_PLUS_PLUS}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_plus_plus "${ERR}")
 add_case(t4_cpp_radius_plus_abc t4_cpp "${RAD_PLUS_ABC}" t4_bad_radius "${USAGE}" CONTAINS t4_radius_plus_abc "${ERR}")
-# плохие точки
+# РїР»РѕС…РёРµ С‚РѕС‡РєРё
 add_case(t4_cpp_symbol_coords t4_cpp "${RAD1}" t4_symbol_coords "${DATA}" CONTAINS t4_symbol_coords "${ERR}")
 add_case(t4_cpp_too_few_coords t4_cpp "${RAD1}" t4_too_few_coords "${DATA}" CONTAINS t4_too_few_coords "${ERR}")
 add_case(t4_cpp_empty_coords t4_cpp "${RAD1}" t4_empty_coords "${NO_INPUT}" CONTAINS t4_empty_coords "${ERR}")
@@ -189,10 +189,10 @@ add_case(t4_cpp_x_ky_z t4_cpp "${RAD1}" t4_x_ky_z "${DATA}" CONTAINS t4_x_ky_z "
 add_case(t4_cpp_x_yk_z t4_cpp "${RAD1}" t4_x_yk_z "${DATA}" CONTAINS t4_x_yk_z "${ERR}")
 add_case(t4_cpp_plus_plus_coord t4_cpp "${RAD1}" t4_plus_plus_coord "${DATA}" CONTAINS t4_plus_plus_coord "${ERR}")
 add_case(t4_cpp_plus_abc_coord t4_cpp "${RAD1}" t4_plus_abc_coord "${DATA}" CONTAINS t4_plus_abc_coord "${ERR}")
-# нормальные данные
+# РЅРѕСЂРјР°Р»СЊРЅС‹Рµ РґР°РЅРЅС‹Рµ
 add_case(t4_cpp_test1 t4_cpp "${RAD8}" t4_test1 "${SUCCESS}" EQ t4_test1 "${OUT}")
 add_case(t4_cpp_test2 t4_cpp "${RAD10}" t4_test2 "${SUCCESS}" EQ t4_test2 "${OUT}")
-# как прошлый тест, но радиус со знаком "+". результат и поведение должны совпасть
+# РєР°Рє РїСЂРѕС€Р»С‹Р№ С‚РµСЃС‚, РЅРѕ СЂР°РґРёСѓСЃ СЃРѕ Р·РЅР°РєРѕРј "+". СЂРµР·СѓР»СЊС‚Р°С‚ Рё РїРѕРІРµРґРµРЅРёРµ РґРѕР»Р¶РЅС‹ СЃРѕРІРїР°СЃС‚СЊ
 add_case(t4_cpp_plus t4_cpp "${RAD_POSITIVE}" t4_test2 "${SUCCESS}" EQ t4_test2 "${OUT}")
 add_case(t4_cpp_test3 t4_cpp "${RAD5}" t4_test3 "${SUCCESS}" EQ t4_test3 "${OUT}")
 add_case(t4_cpp_scientific t4_cpp "${RAD100}" t4_scientific "${SUCCESS}" EQ t4_scientific "${OUT}")
